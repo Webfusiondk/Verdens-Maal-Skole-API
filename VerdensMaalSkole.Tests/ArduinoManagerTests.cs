0ae3715e85a15c6d372810bb4b615a91f01970aa
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-10-05 10:31:50 +0200

    Create ArduinoManagerTests.cs

---
+++ VerdensMaalSkole.Tests/ArduinoManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VerdensMaalSkole.Tests
+{
+    public class ArduinoManagerTests
+    {
+        [Fact]
+        public void template_test()
+        {
+            //Arrange
+
+
+
+            //Act
+
+
+
+            //Assert
+
+
+        }
+    }
+}
