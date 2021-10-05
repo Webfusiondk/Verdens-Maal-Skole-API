db4940310856684611e0efc7cdd9673463f39218
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-10-05 09:40:20 +0200

    Added UnitTesting Library

---
+++ VerdensMaalSkole.Tests/DataAccessTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verdens_Maal_Skole;
+using Xunit;
+
+namespace VerdensMaalSkole.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void GetAllReaders_ShouldReturnData()
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            bool actual = false;
+
+            if (Verdens_Maal_Skole.DataAccess.GetAllReaders() != null)
+            {
+                actual = true;
+            }
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
