739ec87d16dc2052cfe063003ebcfd43534d1261
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-10-12 08:34:12 +0200

    Check for existing token Update

--- Verdens-Maal-Skole/TokenManager.cs
+++ Verdens-Maal-Skole/TokenManager.cs
@@ -9,6 +9,10 @@ namespace Verdens_Maal_Skole
     public class TokenManager
     {
 
+        /// <summary>
+        /// Generates a new token
+        /// </summary>
+        /// <returns></returns>
         public SessionToken GenerateToken()
         {
             string token = "";
@@ -21,9 +25,31 @@ namespace Verdens_Maal_Skole
             return new SessionToken(token);
         }
 
+        /// <summary>
+        /// Updates a valid token's expiration time
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         public int UpdateToken(string token)
         {
             return DataAccess.UpdateSessionToken(token);
+        }
+
+        /// <summary>
+        /// Checks if a token already exists in the database
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public bool CheckForToken(string token)
+        {
+            if(DataAccess.SelectToken(token) != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
